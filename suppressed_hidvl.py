1fbd4db2bde1a2b5a75861e8fd4193c96bf584a8
aprovoNYU <alexandra.provo@nyu.edu>
2018-08-03 11:43:12 -0400

    updates and rerunning script

--- suppressed_hidvl.py
+++ suppressed_hidvl.py
@@ -9,10 +9,15 @@ filetime = datetime.now()
 filetime = filetime.strftime("%Y-%m-%d_%I-%M_%p")
 
 bsn_list_published = []
-bsn_list_published_suppressed = []
+bsn_list_published_aleph = []
+bsn_list_suppressed = []
+bsn_list_hidvlpublished_suppressed = []
 
 count_suppressed=0
 count_published=0
+count_published_aleph=0
+
+
 with open("hidvl_recently_published.csv", "r") as hidvl_recently_published:
 	hidvl_reader = csv.reader(hidvl_recently_published)
 	next(hidvl_reader)
@@ -20,8 +25,8 @@ with open("hidvl_recently_published.csv", "r") as hidvl_recently_published:
 		aleph_bsn_published = row[1].replace("\n", "")
 		bsn_list_published.append(aleph_bsn_published)
 		count_published+=1
-print(bsn_list_published)
-print("count of published records: ",count_published)
+print(set(bsn_list_published))
+print("count of records recently published to HIDVL site: ",count_published)
 
 
 with open("hidvl_20180801.q", 'r') as hidvl_recs:
@@ -32,31 +37,54 @@ with open("hidvl_20180801.q", 'r') as hidvl_recs:
 		handle = ""
 		if "85640 L" in line:
 			handle = re.search(r"(?<=85640 L \$\$u).*(?=\$\$9WEB)",line).group()
-			
-			if handle:
-				pass
-			else:
-				handle = "no handle"
-		#print(aleph_bsn, '| ', handle)		
+			bsn_list_published_aleph.append(aleph_bsn)
+			count_published_aleph+=1
+		#print(aleph_bsn, '| ', handle)
 		#print(line)
-		
-		if "SUPPRESSED" in line and aleph_bsn in bsn_list_published:
+
+		if "SUPPRESSED" in line:
 			#print(aleph_bsn, '| ', handle)
-			bsn_list_published_suppressed.append(aleph_bsn)
+			bsn_list_suppressed.append(aleph_bsn)
 			count_suppressed+=1
-print(bsn_list_published_suppressed)
-print("count of suppressed and published records: ",count_suppressed)
+
+print(set(bsn_list_published_aleph))
+print("count of published aleph records: ",count_published_aleph)
+
+print(set(bsn_list_suppressed))
+print("count of suppressed records: ",count_suppressed)
+
+#print(set(bsn_list_published) & set(bsn_list_published_aleph))
+published_in_both = set(bsn_list_published) & set(bsn_list_published_aleph)
+published_in_both_suppressed = published_in_both.intersection(bsn_list_suppressed)
+#these have handles in aleph but are suppressed
+print("count of suppressed records with handles: ",len(published_in_both_suppressed))
+
+#these do not have handles in aleph but are published on the HIDVL site
+#bsn_list_published AND bsn_list_suppressed AND NOT IN bsn_list_published_aleph
+
+for bsn in bsn_list_published:
+	if bsn in bsn_list_suppressed:
+		if bsn not in bsn_list_published_aleph:
+			print(bsn)
+			bsn_list_hidvlpublished_suppressed.append(bsn)
+
+# lists = []
+# lists.append(bsn_list_suppressed)
+# lists.append(bsn_list_published_aleph)
+# lists.append(bsn_list_published)
 
 
+# bsn_published_suppressed = intersect(set(bsn_list_published),bsn_list_published_aleph,bsn_list_suppressed)
+# print(bsn_published_suppressed)
 
 # published_suppressed = set(bsn_list).intersection(bsn_list_published)
-# print(published_suppressed)
+#print(published_suppressed)
 #print(len(bsn_list_published_suppressed))
 
-with open ("suppressed_hidvl_%s.csv" %filetime, 'w') as file:
+with open ("suppressed__needAleph_handles_hidvl_%s.csv" %filetime, 'w') as file:
 	writer = csv.writer(file)
 	writer.writerow(['aleph_bsn_number'])
-	for x in bsn_list_published_suppressed:
+	for x in bsn_list_hidvlpublished_suppressed:
 		#print(x)
 		#with help from: https://stackoverflow.com/questions/6916542/writing-list-of-strings-to-excel-csv-file-in-python
 		writer.writerow([x],)
